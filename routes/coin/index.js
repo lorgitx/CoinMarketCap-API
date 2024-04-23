8d199b1c123e3694690571cc062703f444b5510b
Carlos Lora <codexlora@gmail.com>
2024-04-22 20:16:46 -0500

    Created GET coin price routeand use JUP API to the return the price with Axio

--- routes/coin/index.js
+++ routes/coin/index.js
@@ -1,9 +1,45 @@
+import axios from 'axios'
+
 export default async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     return 'This are the five coin by marketcap'
   })
-  fastify.get('/:tokenId',async function (request,reply){
-    const {tokenId} = request.params
-    return tokenId;
+  fastify.get('/:tokenId',  function (request, reply) {
+    const { tokenId } = request.params
+
+    const tokenIdPrice =  jupPriceApi(tokenId)
+console.log(22)
+    return tokenIdPrice
   })
+}
+
+
+ function jupPriceApi(tokenId) {
+
+  const jupAPI = 'https://price.jup.ag/v4/price?ids=' + tokenId
+
+  // Realizar una solicitud GET utilizando Axios
+  var finalCoinPrice = axios.get(jupAPI)
+    .then(response => {
+      console.log(33)
+      const coin = response.data.data
+      var coinPrice;
+
+      const keys = Object.keys(coin);
+
+      keys.forEach(key => {
+        coinPrice = coin[key].price
+      })
+      //console.log(coinPrice)
+      return coinPrice
+    })
+    .catch(error => {
+      // Manejar errores de la solicitud
+      console.error('Error al realizar la solicitud:', error);
+      return error
+    });
+
+    console.log(44)
+
+  return finalCoinPrice
 }
