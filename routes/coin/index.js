ef3e6a2150e06a8f2ccb37314594bdbbc5363f6c
Carlos Lora <codexlora@gmail.com>
2024-05-21 17:46:54 -0500

    Top Coins and Single Coin Query accept an Array with the token IDs

--- routes/coin/index.js
+++ routes/coin/index.js
@@ -1,33 +1,50 @@
 //GET Route for individual coin route
 export default async function (fastify, opts) {
+
   fastify.get("/", async function (request, reply) {
-    return "This are the five coin by marketcap";
+
+    //Array with the top 5 five coin solana by marketcap
+    const topCoinsToQuery = [
+      'So11111111111111111111111111111111111111112',
+      '6D7NaB2xsLd7cauWu1wKk6KBsJohJmP2qZH9GEfVi5Ui',
+      '5mbK36SZ7J19An8jFochhQS4of8g6BwUjbeCSxBSoWdp',
+      '7BgBvyjrZX1YKz4oh9mjb8ZScatkkwb8DzFx7LoiVkM3',
+      'FU1q8vJpZNUrmqsciSjp8bAKKidGsLmouB8CBdf8TKQv'
+    ]
+
+    return jupPriceApi(topCoinsToQuery)
   });
+  
   fastify.get("/:tokenId", function (request, reply) {
-    const { tokenId } = request.params;
+    const { tokenId } = request.params
+    const coinToQuery = [tokenId]
 
     //Promise that return the coin price from JUP
-    return jupPriceApi(tokenId);
+    return jupPriceApi(coinToQuery);
   });
 }
 
 //Fetch the coin price with tokenId on Solana
 function jupPriceApi(tokenId) {
 
-  const jupAPI = "https://price.jup.ag/v4/price?ids=" + tokenId;
+  var jupAPI = "https://price.jup.ag/v4/price?ids=" 
+  tokenId.forEach((element) => {jupAPI =jupAPI+','+element})  
 
   // Fetch to  JUP API
   var finalCoinPrice = fetch(jupAPI)
     .then((response) => response.json())
 
-    .then((data) => {
-      const coin = data.data;
-      var coinPrice;
+    .then((responseJSON) => {
 
+      const coin = responseJSON.data;
+
+      console.log(coin) 
+
+      var coinPrice = [];
       const keys = Object.keys(coin);
 
       keys.forEach((key) => {
-        coinPrice = coin[key].price;
+        coinPrice.push(coin[key].id+':'+coin[key].price);
       });
 
       return coinPrice;
