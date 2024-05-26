8950abb60a4385a9c9f2c1593ed1f43677330794
Carlos Lora <codexlora@gmail.com>
2024-05-26 18:10:59 -0500

    Added a section with the token data

--- client/assets/js/script.js
+++ client/assets/js/script.js
@@ -1,20 +1,44 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  async function GetPrices() {
-    const request = await fetch("http://127.0.0.1:4000/coin/price/");
-    const prices = await JSON.parse(await request.text());
-
-    if (document.querySelector("ul")) document.querySelector("ul").remove();
-
-    const ul = document.createElement("ul");
-
-    for (const token of prices) {
-      ul.insertAdjacentHTML("beforeend", `<li>${token}</li>`);
-    }
-
-    document.querySelector("h1").insertAdjacentElement("afterend", ul);
-  }
-
   GetPrices();
-
+  GetTokens();
   setInterval(GetPrices, 5000);
 });
+
+async function GetPrices() {
+  const request = await fetch("http://127.0.0.1:4000/coin/price/");
+  const prices = await JSON.parse(await request.text());
+
+  if (document.querySelector(".trending-coins ul"))
+    document.querySelector(".trending-coins ul").remove();
+
+  const ul = document.createElement("ul");
+
+  for (const token of prices) {
+    ul.insertAdjacentHTML("beforeend", `<li>${token}</li>`);
+  }
+
+  document
+    .querySelector(".trending-coins")
+    .insertAdjacentElement("afterbegin", ul);
+}
+
+async function GetTokens() {
+  const request = await fetch("http://127.0.0.1:4000/coin/data/list/");
+  const tokens = await JSON.parse(await request.text());
+
+  if (document.querySelector(".stored-coins ul"))
+    document.querySelector(".stored-coins ul").remove();
+
+  const ul = document.createElement("ul");
+
+  for (const token in tokens) {
+    ul.insertAdjacentHTML(
+      "beforeend",
+      `<li>${tokens[token].tokenName} : ${tokens[token].tokenPublicID}</li>`
+    );
+  }
+
+  document
+    .querySelector(".stored-coins")
+    .insertAdjacentElement("afterbegin", ul);
+}
