2a4bd3348435dbef3a2c02cf344ae24522f355ed
Carlos Lora <codexlora@gmail.com>
2024-05-26 01:46:23 -0500

    Now all the plugins and routes  come from a src folder loaded with Fastify Autoloader

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
-import fastifyAutoload from '@fastify/autoload';
-import { fileURLToPath } from 'url'
-import { dirname, join } from 'path'
-import Fastify from 'fastify'
+import fastifyAutoload from "@fastify/autoload";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import Fastify from "fastify";
 
 //Get the the root folder
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Inside of Fastify object you can write configuration for app
 const app = Fastify({
-  logger: true // Enable logger
+  logger: true, // Enable logger
 });
 
-//Register new AutoLoadPlugin to load all the routes inside the folder structure
+// Autoload pLugins and routes
 app.register(fastifyAutoload, {
-  dir: join(__dirname, 'routes')
-})
-
-app.register(fastifyAutoload,{
-  dir: join(__dirname,'plugins')
-})
+  dir: join(__dirname, "src"),
+  dirNameRoutePrefix: function rewrite(folderParent, folderName) {
+    if (folderName === "coin") {
+      return folderName;
+    }
+    return false;
+  },
+});
 
 // Run web server
 try {
-  await app.listen({ port: 4000 })
+  await app.listen({ port: 4000 });
 } catch (err) {
-  app.log.error(err)
-  process.exit(1)
+  app.log.error(err);
+  process.exit(1);
 }
