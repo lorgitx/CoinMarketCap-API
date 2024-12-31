2982e73ca5ec4ef3c4d46a50f6d936ee8b35f833
Carlos Lora <lorgitx@gmail.com>
2024-12-31 13:43:11 -0500

    Enable CORS Plugin

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import { dirname, join } from "path";
 import Fastify from "fastify";
 import fastifyAutoload from "@fastify/autoload";
 
+import fastifyCors from '@fastify/cors';
+
 //Get the the root folder
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +13,12 @@ const __dirname = dirname(__filename);
 // Inside of Fastify object you can write configuration for app
 const app = Fastify({
   logger: true, // Enable logger
+});
+
+// Registrar el plugin CORS
+app.register(fastifyCors, {
+  origin: '*',  // Puedes especificar un dominio específico en lugar de '*'
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Métodos permitidos
 });
 
 // Autoload pLugins and routes
