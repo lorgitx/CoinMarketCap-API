cb6176b3cae9782efc677713dbdabf0eaedd8ed9
Carlos Lora <lorgitx@gmail.com>
2025-02-02 18:22:37 -0500

    Converted to Serverless, Static Plugin Removed

--- server.js
+++ server.js
@@ -10,22 +10,23 @@ if (!isProduction && !isPreview) {
   console.log('Using Vercel .env');
 }
 
-// import { fileURLToPath } from "url";
-// import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
-// import Fastify from "fastify";
-// import fastifyAutoload from "@fastify/autoload";
+import Fastify from "fastify";
+
+import fastifyAutoload from "@fastify/autoload";
 
 // import fastifyCors from '@fastify/cors';
 
 // //Get the the root folder
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// // Inside of Fastify object you can write configuration for app
-// const app = Fastify({
-//   logger: true, // Enable logger
-// });
+// Inside of Fastify object you can write configuration for app
+const app = Fastify({
+  logger: true, // Enable logger
+});
 
 // // Registrar el plugin CORS
 // app.register(fastifyCors, {
@@ -33,21 +34,24 @@ if (!isProduction && !isPreview) {
 //   methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Métodos permitidos
 // });
 
-// // Autoload pLugins and routes
-// app.register(fastifyAutoload, {
-//   dir: join(__dirname, "src"),
-//   dirNameRoutePrefix: function rewrite(folderParent, folderName) {
-//     if (folderName === "coin") {
-//       return folderName;
-//     }
-//     return false;
-//   },
-// });
+// Autoload pLugins and routes
+app.register(fastifyAutoload, {
+  dir: join(__dirname, "src"),
+  dirNameRoutePrefix: function rewrite(folderParent, folderName) {
+    if (folderName === "coin") {
+      return folderName;
+    }
+    return false;
+  },
+});
 
-// // Run web server
-// try {
-//   await app.listen({ port: 4000 });
-// } catch (err) {
-//   app.log.error(err);
-//   process.exit(1);
-// }
+export default async (req, res) => {
+  // Run web server
+  try {
+    await app.ready();
+    app.server.emit('request', req, res);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+} 
