f1737ab966b1780360984fc903193aa3f4f19367
Carlos Lora <lorgitx@gmail.com>
2025-02-02 16:21:12 -0500

    Conditional to load a custom .env on local

--- server.js
+++ server.js
@@ -1,41 +1,53 @@
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
+import { config } from 'dotenv';
 
-import Fastify from "fastify";
-import fastifyAutoload from "@fastify/autoload";
+const isProduction = process.env.VERCEL_ENV === 'production';
+const isPreview = process.env.VERCEL_ENV === 'preview';
 
-import fastifyCors from '@fastify/cors';
-
-//Get the the root folder
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Inside of Fastify object you can write configuration for app
-const app = Fastify({
-  logger: true, // Enable logger
-});
-
-// Registrar el plugin CORS
-app.register(fastifyCors, {
-  origin: '*',  // Puedes especificar un dominio específico en lugar de '*'
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Métodos permitidos
-});
-
-// Autoload pLugins and routes
-app.register(fastifyAutoload, {
-  dir: join(__dirname, "src"),
-  dirNameRoutePrefix: function rewrite(folderParent, folderName) {
-    if (folderName === "coin") {
-      return folderName;
-    }
-    return false;
-  },
-});
-
-// Run web server
-try {
-  await app.listen({ port: 4000 });
-} catch (err) {
-  app.log.error(err);
-  process.exit(1);
+if (!isProduction && !isPreview) {
+  console.log('Loading custom .env');
+  config({ path: '.env.dev' });
+} else {
+  console.log('Using Vercel .env');
 }
+
+// import { fileURLToPath } from "url";
+// import { dirname, join } from "path";
+
+// import Fastify from "fastify";
+// import fastifyAutoload from "@fastify/autoload";
+
+// import fastifyCors from '@fastify/cors';
+
+// //Get the the root folder
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// // Inside of Fastify object you can write configuration for app
+// const app = Fastify({
+//   logger: true, // Enable logger
+// });
+
+// // Registrar el plugin CORS
+// app.register(fastifyCors, {
+//   origin: '*',  // Puedes especificar un dominio específico en lugar de '*'
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Métodos permitidos
+// });
+
+// // Autoload pLugins and routes
+// app.register(fastifyAutoload, {
+//   dir: join(__dirname, "src"),
+//   dirNameRoutePrefix: function rewrite(folderParent, folderName) {
+//     if (folderName === "coin") {
+//       return folderName;
+//     }
+//     return false;
+//   },
+// });
+
+// // Run web server
+// try {
+//   await app.listen({ port: 4000 });
+// } catch (err) {
+//   app.log.error(err);
+//   process.exit(1);
+// }
