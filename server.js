dc804b8bcba0290464150b3480cc5952ed246b09
Carlos Lora <lorgitx@gmail.com>
2025-02-03 21:42:46 -0500

    Added hook

--- server.js
+++ server.js
@@ -45,6 +45,14 @@ app.register(fastifyAutoload, {
   },
 });
 
+app.addHook('preParsing', (request, reply, payload, done) => {
+  // Si la URL empieza con "/api", la modificamos quitándole ese prefijo
+  if (request.raw.url.startsWith('/api')) {
+    request.raw.url = request.raw.url.slice(4); // quita los primeros 4 caracteres: "/api"
+  }
+  done(null, payload);
+});
+
 
 export default async (req, res) => {
   // Run web server
